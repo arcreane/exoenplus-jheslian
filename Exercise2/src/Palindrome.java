5fd5da6d44451a20a8e145aa709c2d14764cecc1
jesusa <justinjhes@gmail.com>
2020-11-02 22:45:17 +0100

    creation de menu

--- Exercise2/src/Palindrome.java
+++ Exercise2/src/Palindrome.java
@@ -4,21 +4,26 @@ public class Palindrome {
 
     public static String word;
 
-        public static void main (String[]args) {
-            Scanner in = new Scanner( System.in );
-
+    public static void palindrome() {
+        Scanner in = new Scanner( System.in );
+        while (true) {
             System.out.println( "Enter a Palindrome word" );
             word = in.nextLine();
+            if (word.equals("0"))
+                Menu.main(null);
+
             String reverse = "";
-            char[] letters = word.toCharArray();
             for (int i = word.length() - 1; i >= 0; i--) {
                 reverse += word.charAt( i );
-                //System.out.println(reverse);
             }
 
             if (word.equals( reverse ))
-                System.out.println(word+  " is a Palindrome" );
+                System.out.println( word + " is a Palindrome" );
             else
                 System.out.println( word + " is not a Palindrome word/s" );
+
+            System.out.print("Enter 0 to return to Menu or ");
         }
+    }
 }
+
